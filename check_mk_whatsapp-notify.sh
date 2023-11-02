6180883f52636a6f1845a15c0080bec3a78de60c
Welligton Alves - Linux4Life <welligtonalves.ti@gmail.com>
2023-11-02 17:38:46 +0000

    Update check_mk_whatsapp-notify.sh

--- check_mk_whatsapp-notify.sh
+++ check_mk_whatsapp-notify.sh
@@ -1,89 +1,93 @@
 #!/bin/bash
-# Push Notification (using whatsapp)
+# Push Notification (using WhatsApp)
 #
 # Script Name   : check_mk_whatsapp-notify.sh
-# Description   : Send Check_MK notifications by WhatsApp
+# Description   : Send Check_MK notifications via WhatsApp
 # Author        : Welligton Analista Linux4Life
 # License       : BSD 3-Clause "New" or "Revised" License
 # ======================================================================================
 
-#Variables
+# Variáveis
 
-COMAND1="curl --user user:api --location --request -S -X POST"
-COMAND2="--data "
+COMMAND="curl --user user:api --location --request -S -X POST"
 
-#WhatsApp API Token, Port, Line and Destination
+# Token da API do WhatsApp, Porta, Linha e Destino
 
-if [ -z ${NOTIFY_PARAMETER_1} ]; then
-        echo "No WhatsApp Line. Exiting" >&2
-        exit 2
+if [ -z "${NOTIFY_PARAMETER_1}" ]; then
+    echo "Nenhuma linha do WhatsApp definida. Saindo." >&2
+    exit 2
 else
-        line="${NOTIFY_PARAMETER_1}"
+    line="${NOTIFY_PARAMETER_1}"
 fi
 
-if [ -z ${NOTIFY_PARAMETER_2} ]; then
-        echo "No WhatsApp destiny. Exiting" >&2
-        exit 2
+if [ -z "${NOTIFY_PARAMETER_2}" ]; then
+    echo "Nenhum destino do WhatsApp definido. Saindo." >&2
+    exit 2
 else
-        destiny="${NOTIFY_PARAMETER_2}"
+    destiny="${NOTIFY_PARAMETER_2}"
 fi
 
-if [ -z ${NOTIFY_PARAMETER_3} ]; then
-        echo "No WhatsApp port. Exiting" >&2
-        exit 2
+if [ -z "${NOTIFY_PARAMETER_3}" ]; then
+    echo "Nenhuma porta do WhatsApp definida. Saindo." >&2
+    exit 2
 else
-        port="${NOTIFY_PARAMETER_3}"
+    port="${NOTIFY_PARAMETER_3}"
 fi
 
-if [ -z ${NOTIFY_PARAMETER_4} ]; then
-        echo "No WhatsApp key. Exiting" >&2
-        exit 2
+if [ -z "${NOTIFY_PARAMETER_4}" ]; then
+    echo "Nenhuma chave do WhatsApp definida. Saindo." >&2
+    exit 2
 else
-        key="${NOTIFY_PARAMETER_4}"
+    key="${NOTIFY_PARAMETER_4}"
 fi
 
-# Set an appropriate emoji for the current state
+# Defina um emoji apropriado para o estado atual
 
-if [[ ${NOTIFY_WHAT} == "SERVICE" ]]; then
-        STATE="${NOTIFY_SERVICESHORTSTATE}"
+if [[ "${NOTIFY_WHAT}" == "SERVICE" ]]; then
+    STATE="${NOTIFY_SERVICESHORTSTATE}"
 else
-        STATE="${NOTIFY_HOSTSHORTSTATE}"
+    STATE="${NOTIFY_HOSTSHORTSTATE}"
 fi
+
 case "${STATE}" in
     OK|UP)
-        EMOJI=$'\u2705' 
+        EMOJI=$'\u2705'  # Emoji de marca de seleção verde
         ;;
     WARN)
-        EMOJI=$'\u26A0\uFE0F' 
+        EMOJI=$'\u26A0\uFE0F'  # Emoji de aviso
         ;;
     CRIT|DOWN)
-        EMOJI=$'\u274C' 
+        EMOJI=$'\u274C'  # Emoji de marca de seleção vermelha
         ;;
     UNKN)
-        EMOJI=$'U+1F612' 
+        EMOJI=$'\u2753'  # Emoji de ponto de interrogação
+        ;;
 esac
 
-# Create a MESSAGE variable to send to your WhatsApp
+# Crie uma variável MESSAGE para enviar ao seu WhatsApp
 
 MESSAGE="${NOTIFY_HOSTNAME} (${NOTIFY_HOSTALIAS})\n\n"
 MESSAGE+="${EMOJI} ${NOTIFY_WHAT} ${NOTIFY_NOTIFICATIONTYPE}\n\n"
-if [[ ${NOTIFY_WHAT} == "SERVICE" ]]; then
-        MESSAGE+="${NOTIFY_SERVICEDESC}\n"
-        MESSAGE+="State changed from ${NOTIFY_PREVIOUSSERVICEHARDSHORTSTATE} to ${NOTIFY_SERVICESHORTSTATE}\n"
-        MESSAGE+="${NOTIFY_SERVICEOUTPUT}\n"
+
+if [[ "${NOTIFY_WHAT}" == "SERVICE" ]]; then
+    MESSAGE+="${NOTIFY_SERVICEDESC}\n"
+    MESSAGE+="Estado alterado de ${NOTIFY_PREVIOUSSERVICEHARDSHORTSTATE} para ${NOTIFY_SERVICESHORTSTATE}\n"
+    MESSAGE+="${NOTIFY_SERVICEOUTPUT}\n"
 else
-        MESSAGE+="State changed from ${NOTIFY_PREVIOUSHOSTHARDSHORTSTATE} to ${NOTIFY_HOSTSHORTSTATE}\n"
-        MESSAGE+="${NOTIFY_HOSTOUTPUT}\n"
+    MESSAGE+="Estado alterado de ${NOTIFY_PREVIOUSHOSTHARDSHORTSTATE} para ${NOTIFY_HOSTSHORTSTATE}\n"
+    MESSAGE+="${NOTIFY_HOSTOUTPUT}\n"
 fi
 
 MESSAGE+="\nIPv4: ${NOTIFY_HOST_ADDRESS_4} \nIPv6: ${NOTIFY_HOST_ADDRESS_6}\n"
 MESSAGE+="${NOTIFY_SHORTDATETIME} | ${OMD_SITE}"
 
-# Send message to WhatsApp bot
-$COMAND1 "http://api.meuaplicativo.vip:${port}/services/message_send?line=${line}&destiny=${destiny}&reference&text=${MESSAGE}" $COMAND2 "App=NetiZap%20Consumers%201.0&AccessKey=${key}"
+# Envie a mensagem para o bot do WhatsApp
+
+$COMMAND "http://api.meuaplicativo.vip:${port}/services/message_send?line=${line}&destiny=${destiny}&reference&text=${MESSAGE}" --data "App=NetiZap%20Consumers%201.0&AccessKey=${key}"
+
 if [ $? -ne 0 ]; then
-        echo "Not able to send WhatsApp message" >&2
-        exit 2
+    echo "Não foi possível enviar a mensagem do WhatsApp" >&2
+    exit 2
 else
-        exit 0
+    exit 0
 fi
